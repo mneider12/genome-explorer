fe72fa27a8050ce708f3809f2881a5f461290d5d
Nick Daly <nick.m.daly@gmail.com>
2016-10-04 08:32:01 -0500

    Reorganized: easier to add additional schemas.
    importSchema is now a single function call per schema file, like it should've been in the first place.

--- src/createDb.sh
+++ src/createDb.sh
@@ -6,12 +6,29 @@ howMuch=-0
 
 importTables() {
 # do the normal table import after setting up the database
-    importSnp "ftp://ftp.ncbi.nih.gov/snp/database/shared_data/Allele.bcp.gz"
+    importSnp "ftp://ftp.ncbi.nih.gov/snp/database/shared_data/Allele.bcp.gz" # syntax error before 500k lines.
     importSnp "ftp://ftp.ncbi.nih.gov/snp/database/organism_data/human_9606/ClinSigCode.bcp.gz"
     importSnp "ftp://ftp.ncbi.nih.gov/snp/database/organism_data/human_9606/Pedigree.bcp.gz"
     importSnp "ftp://ftp.ncbi.nih.gov/snp/database/organism_data/human_9606/SNPClinSig.bcp.gz"
     importSnp "ftp://ftp.ncbi.nih.gov/snp/database/organism_data/human_9606/SNPPubmed.bcp.gz"
     importSnp "ftp://ftp.ncbi.nih.gov/snp/database/organism_data/human_9606/Synonym.bcp.gz"
+}
+importSchemas() {
+    # import the schemas
+    # importSchema "ftp://fileName"
+    importSchema "ftp://ftp.ncbi.nih.gov/snp/database/shared_schema/dbSNP_main_table.sql.gz"
+
+    # TODO add the index files and whatnot here.
+
+
+    # manually exclude the syntax erroring empty table SubSNPOmim.
+    download "ftp://ftp.ncbi.nih.gov/snp/database/organism_schema/human_9606/human_9606_table.sql.gz"
+    echo -n "Importing human schema... "
+    cat "${dbDir}/${fileName}" | \
+        gunzip | \
+        sed -e "/SubSNPOmim/,+4d" | \
+        sqlite3 $dbFile
+    echo "Done."
 }
 
 
@@ -21,10 +38,25 @@ importTables() {
 dbDir=bin
 dbFile=$dbDir/project.sqlite
 
-#
-# functions
-#
+setup() {
+# blow away the previous database.
+    mkdir -p $dbDir
+    rm $dbFile* 2>&1 > /dev/null
+
+    importSchemas
+}
+importSchema() {
+# import named schema file from bin directory.
+    download $1
+    
+    echo -n "Importing schema ${fileName}... "
+    cat "${dbDir}/${fileName}" | \
+        gunzip | \
+        sqlite3 $dbFile
+    echo "Done."
+}
 download() {
+# download named file to bin directory.
     fileName=${1##*\/}
 
     echo -n "Downloading ${fileName}... "
@@ -33,40 +65,6 @@ download() {
     wget -qNc $1
     cd $here
     echo "Done."
-}
-setup() {
-# blow away the previous database.
-    mkdir -p $dbDir
-    rm $dbFile* 2>&1 > /dev/null
-
-    # import the schemas
-    download "ftp://ftp.ncbi.nih.gov/snp/database/shared_schema/dbSNP_main_table.sql.gz"
-    echo -n "Importing shared schema... "
-    cat "${dbDir}/${fileName}" | \
-        gunzip | \
-        sqlite3 $dbFile
-    echo "Done."
-
-    # exclude the syntax erroring empty table SubSNPOmim.
-    download "ftp://ftp.ncbi.nih.gov/snp/database/organism_schema/human_9606/human_9606_table.sql.gz"
-    echo -n "Importing human schema... "
-    cat "${dbDir}/${fileName}" | \
-        gunzip | \
-        sed -e "/SubSNPOmim/,+4d" | \
-        sqlite3 $dbFile
-    echo "Done."
-
-}
-makeImport () {
-# create import file to import into correct table name.
-    fileName=${1##*\/}
-    tablename=${fileName%%.*}
-    importCommand=$(mktemp)
-
-    cat <<EOF > $importCommand
-.mode tabs $tablename
-.import /dev/stdin $tablename
-EOF
 }
 importSnp () {
 # import a particular table.
@@ -82,6 +80,17 @@ importSnp () {
         head -n $howMuch | \
         sqlite3 --init $importCommand $dbFile
     echo "Done."
+}
+makeImport () {
+# create import file to import into correct table name.
+    fileName=${1##*\/}
+    tablename=${fileName%%.*}
+    importCommand=$(mktemp)
+
+    cat <<EOF > $importCommand
+.mode tabs $tablename
+.import /dev/stdin $tablename
+EOF
 }
 
 #
