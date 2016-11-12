a5bbc434a254badd7d3a65b374660c59aa7c7252
Nick Daly <Nick.M.Daly@gmail.com>
2016-11-12 11:09:43 -0600

    Make ER diagram easier to read.
    Switch to left-to-right hierarchical view.  Significantly easier to
    read because our ER diagram is very tree-like.

--- src/createErDiagram.sh
+++ src/createErDiagram.sh
@@ -2,4 +2,4 @@
 
 # make an ER diagram.
 
-neato -Tpng -Goverlap=prism "src/Genetic Database ER diagram.dot" > "bin/Genetic Database ER diagram.png"
+dot -Tpng -Goverlap=prism "src/Genetic Database ER diagram.dot" > "bin/Genetic Database ER diagram.png"
